86e6be251efb50df4edfc8423e915f403c7fc824
BjornWest <bpf.westerlund@gmail.com>
2023-02-21 14:23:42 +0100

    test commit

---
+++ src/algo_collection.ts
@@ -0,0 +1,17 @@
+import { type SquareMatrix, CompleteGraph, type Edge } from "./graph";
+
+type Path<T> = {items: Array<T>};
+
+
+/*
+using memoization
+
+*/
+
+function add_mock<T>(graph: CompleteGraph<T>): CompleteGraph<T>{
+    graph.weight_matrix
+}
+
+function Held_Karp<T>(graph: CompleteGraph<T>): Path<T> {
+
+}
