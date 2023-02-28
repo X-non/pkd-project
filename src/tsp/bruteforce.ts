4fd3378a39b045f46cccc22d98e1375770aa4d7f
Casper <casper.bjorkman@gmail.com>
2023-02-28 14:32:53 +0100

    added implementation

--- src/tsp/bruteforce.ts
+++ src/tsp/bruteforce.ts
@@ -1,10 +1,26 @@
 import { CompleteGraph } from "../graph";
+import { all_permutations_lazy, cyclic_path_length } from "./utils";
 
 
 
+/**
+ * Finds the shortest hamiltionian path through all nodes in
+ * the provided graph
+ * @param graph - the graph we want to find the shortest hamiltionian path  
+ */
+export function tsp_naive<T>(graph: CompleteGraph<T>): Array<number> {
+    let shortest_path_length = Infinity;
+    let shortest_path: Array<number> = [];
+    const all_permutations = all_permutations_lazy(graph.all_nodes());
 
-function tspbrute<T>(graph: CompleteGraph<T>,) {
-    let longest = 0;
-    let shortest = Infinity;
+    for (const current_path of all_permutations) {
+        const path_length = cyclic_path_length(graph, current_path);
 
+        if (path_length < shortest_path_length) {
+            shortest_path = current_path;
+            shortest_path_length = path_length;
+        }
+    }
+
+    return shortest_path;
 }
